496f81b54acddf4a6bb7e865a21a1e8d853095ac
Sudeep S <sudeep.sunthankar@gmail.com>
2017-10-30 13:24:58 +1100

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,11 +3,11 @@ import org.junit.*;
 public class MyTest {
     @Test
     public void test_method_1() {
-        System.out.println("test_method_1");
+        System.out.println("Test_method_1");
     }
 
     @Test
     public void test_method_2() {
-        System.out.println("test_method_1");
+        System.out.println("Test_method_2");
     }
 }
